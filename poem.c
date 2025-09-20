681ceea6771fd122398c443b31aef3305266a380
Jhanpiere Montes <jhanpieremontes@gmail.com>
2025-09-20 13:02:58 -0500

    Se agregaron los primeros archivos

---
+++ poem.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(void)
+{
+printf("EL POETA A SU AMADA\n"
+    "Amada, en esta noche tu te has crucificado\n"
+    "sobre los dos maderos curvados de mi beso;\n"
+    "y tu pena me ha dicho que Jesus ha llorado,\n"
+    "y que hay un viernes santo mas dulce que ese beso.\n"
+    "\n"
+    "En esta noche clara que tanto me has mirado,\n"
+    "la Muerte ha estado alegre y ha cantado en su hueso.\n"
+    "En esta noche de setiembre se ha oficiado\n"
+    "mi segunda caida y el mas humano beso.\n"
+    "\n"
+    "Amada, moriremos los dos juntos, muy juntos;\n"
+    "se ira secando a pausas nuestra excelsa amargura;\n"
+    "y habran tocado a sombra nuestros labios difuntos.\n"
+    "\n"
+    "Y ya no habra reproches en tus ojos benditos;\n"
+    "ni volvere a ofenderte. Y en una sepultura\n"
+    "los dos nos dormiremos, como dos hermanitos.");
+    return 0;
+}
